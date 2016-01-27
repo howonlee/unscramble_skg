4e9bb1995b0358f4603b81b58523c86c1dfc055a
Howon Lee <howon@howonlee.com>
2016-01-26 18:29:19 -0800

    rounding wrong

--- 1d.py
+++ 1d.py
@@ -1,4 +1,7 @@
 import numpy as np
+import math
+import operator as op
+import sys
 import numpy.random as npr
 import matplotlib.pyplot as plt
 from heapdict import heapdict
@@ -23,24 +26,42 @@ def fractal_attractor():
     plt.plot(sorted(data))
     plt.show()
 
-def frac_ordering(length):
-############
-############
-############
-############
-    pass
+done_already = set()
+
+def frac_ordering(prev, total):
+    if prev == -1:
+        return -1
+    done_already.add(prev)
+    new_member = int(math.ceil(prev / 2.0))
+    if new_member in done_already:
+        new_member = int(math.ceil((prev + total) / 2.0))
+        if new_member in done_already: #again
+            return -1
+    return new_member
 
 def unshuffle(to_unshuffle):
     hd = heapdict()
     for x in xrange(to_unshuffle.size):
+        # hd[x] = -to_unshuffle[x] - (x / 25600.00)
         hd[x] = -to_unshuffle[x]
+    items_ordered = []
     while hd.keys():
-        print hd.popitem()
+        items_ordered.append(hd.popitem())
+    return items_ordered
+
+def test_frac_ordering():
+    currs = []
+    curr = 0
+    for x in xrange(64):
+        currs.append(curr)
+        curr = frac_ordering(curr, 64)
+    return currs
 
 if __name__ == "__main__":
-    data = kron_line(8)
-    npr.shuffle(data)
-    unshuffle(data)
+    print sorted(test_frac_ordering()[7:22])
+    data = kron_line(6)
+    # npr.shuffle(data)
+    print sorted(unshuffle(data)[7:22], key=op.itemgetter(0))
     # unshuffled_data = unshuffle(data)
     # plt.plot(data)
     # plt.plot(unshuffled_data)
