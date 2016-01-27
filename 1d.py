b1c09e728633bb980cb87c1f624ed78bf2db7edb
Howon Lee <howon@howonlee.com>
2016-01-26 17:22:22 -0800

    unscrambling thoughts

--- 1d.py
+++ 1d.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.random as npr
 import matplotlib.pyplot as plt
+from heapdict import heapdict
 
 def kron_line(order):
     generator = np.array([1.0, 0.5])
@@ -9,6 +10,30 @@ def kron_line(order):
         arr = np.kron(arr, generator)
     return arr
 
-if __name__ == "__main__":
-    plt.plot(kron_line(10))
+def fractal_attractor():
+    """
+    Interesting phenomenon, but not unscrambling
+    """
+    data = kron_line(15)
+    unshuffled_data = data.copy()
+    npr.shuffle(data) # inplace
+    for x in xrange(1000):
+        npr.shuffle(data)
+        data = np.abs(data - unshuffled_data)
+    plt.plot(sorted(data))
     plt.show()
+
+def unshuffle(to_unshuffle):
+    hd = heapdict()
+    for x in xrange(to_unshuffle.size):
+        hd[x] = to_unshuffle[x]
+    print hd.values()
+
+if __name__ == "__main__":
+    data = kron_line(8)
+    npr.shuffle(data)
+    unshuffle(data)
+    # unshuffled_data = unshuffle(data)
+    # plt.plot(data)
+    # plt.plot(unshuffled_data)
+    # plt.show()
